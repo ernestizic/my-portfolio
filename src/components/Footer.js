670c0f4e411630e55180409930b4bd7367a1828a
Ernest Isaac <ieifeanyichukwu@gmail.com>
2021-11-25 15:19:53 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export const Footer = () => {
                 <p> <a href='http://twitter.com/ernestizic'><AiFillTwitterCircle /></a> </p>
                 <p> <a href='https://www.linkedin.com/in/isaac-ifeanyichukwu-9b0039205/'><FaLinkedin /></a> -------- </p>
             </div>
-            <p><code style={{color: '#f0f0f0', fontSize: '15px'}}>Designed & Built by Isaac I. Ernest</code></p>
+            <p><code style={{color: '#f0f0f0', fontSize: '15px'}}>Designed & Built by Ernestizic</code></p>
             <p style={{color: '#f0f0f0'}}>&copy; {(new Date().getFullYear())} <code style={{color: '#f0f0f0', fontSize: '15px'}}> Ernestizic. All Rights Reserved</code></p>
         </div>
     )
