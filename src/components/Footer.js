ff3c0865b947c1995cef5c50c71781f5c564a6d5
Ernest Isaac <ieifeanyichukwu@gmail.com>
2021-12-06 13:25:25 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export const Footer = () => {
             <div className='socials'>
                 <p> -------- <a href='https://github.com/ernestizic'><FaGithub /></a> </p>
                 <p> <a href='http://twitter.com/ernestizic'><AiFillTwitterCircle /></a> </p>
-                <p> <a href='https://www.linkedin.com/in/isaac-ifeanyichukwu-9b0039205/'><FaLinkedin /></a> -------- </p>
+                <p> <a href='https://www.linkedin.com/in/ernestizic'><FaLinkedin /></a> -------- </p>
             </div>
             <p><code style={{color: '#f0f0f0', fontSize: '15px'}}>Designed & Built by Ernestizic</code></p>
             <p style={{color: '#f0f0f0'}}>&copy; {(new Date().getFullYear())} <code style={{color: '#f0f0f0', fontSize: '15px'}}> Ernestizic. All Rights Reserved</code></p>
